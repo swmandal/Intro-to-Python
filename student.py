9b8e7c75bae619876e03ab58dcc4e431a23b12db
Swarupa Nanda Mandal <56360810+swmandal@users.noreply.github.com>
2020-02-18 22:05:25 -0600

    Create student.py

---
+++ student.py
@@ -0,0 +1,12 @@
+# student.py 
+# Feb/18/2020
+# swarup
+
+# string1 = "I am a student"
+# string2 = in the biology department.
+# after corrections
+string1 ="I am a student"
+string2 ="in the biology department."
+# joining 
+string3 = (string1) + " " + (string2)
+print (string3)
